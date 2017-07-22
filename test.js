22a16469fc202fcf886f19969c86716173b21c32
Randy Wressell <randy5235@gmail.com>
2017-07-21 23:59:39 -0700

    Add method spy

--- test.js
+++ test.js
@@ -2,10 +2,24 @@ const sinon = require('sinon');
 
 const logger = message => console.log(message);
 
+const user = {
+  setName(name) {
+    name
+  }
+};
+
 it('should call logger once', function() {
   var logger = sinon.spy(logger);
   logger("test");
   //logger.restore();
   sinon.assert.calledOnce(logger);
   sinon.assert.calledWith(logger, "test");
+});
+
+it('should call user.setName once', function() {
+  var spy = sinon.spy(user,'setName');
+  user.setName("Randy");
+  sinon.assert.calledOnce(user.setName);
+  sinon.assert.calledWith(user.setName, "Randy");
+
 });
